82dcf58f54d6079e95178867060d2141e47d93ba
2jongoni <jw20050118@gmail.com>
2025-10-28 17:20:12 +0900

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "dksekrud"
+include(":app")
+include(":app:w02")
+include(":app:w02:w03")
+include(":app:w02:w04")
+include(":app:w03")
+include(":app:w04")
+include(":app:w05")
+include(":app:w06")
+include(":app:w07")
